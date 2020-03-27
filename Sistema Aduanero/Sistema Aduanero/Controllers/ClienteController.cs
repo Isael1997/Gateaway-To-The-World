b912cbda12a8e6f44b427b7b0d90eb86515fc373
gersondevx <gerson.santosm3@gmail.com>
2020-03-27 11:50:55 -0400

    Se agrego el controlador: "ClienteController" y este trabajara directamente con las ventanas del cliente con las acciones:
    ~ Perfil, para el perfil del cliente.
    ~ Listado_De_Servicios_Contratados
    ~ Solicitar_Servicio_De_Importacion_O_Exportacion

---
+++ Sistema Aduanero/Sistema Aduanero/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_Aduanero.Controllers;
+
+namespace Sistema_Aduanero.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ClienteController()
+        {
+            //...
+        }
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+        public IActionResult Listado_De_Servicios_Contratados()
+        {
+            return View();
+        }
+        public IActionResult Solicitar_Servicio_De_Importacion_O_Exportacion()
+        {
+            return View();
+        }
+    }
+}
