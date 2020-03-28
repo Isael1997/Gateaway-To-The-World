1399b22bc274905427772045e11a1aabaff01fe3
gersondevx <gerson.santosm3@gmail.com>
2020-03-28 08:42:42 -0400

    Se le agrego al botón "Nuevo Registro" las acciones de ocultar y mostrar el formulario.

--- Sistema Aduanero/Sistema Aduanero/wwwroot/js/site.js
+++ Sistema Aduanero/Sistema Aduanero/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Este es el bloque de código que trabaja el boton mostrar y ocultar.
+function mostrar_y_ocultar(){
+    var formulario = document.getElementById('formulario_nuevo_registro');
+    var formularioStyle = window.getComputedStyle(formulario);
+    var formularioDisplay = formularioStyle.getPropertyValue('display');
+    if(formularioDisplay == "none"){
+        document.getElementById('formulario_nuevo_registro').style.display = "block";
+    }else{
+        document.getElementById('formulario_nuevo_registro').style.display = "none";
+    }
+}
+//Este es el bloque de código que trabaja el boton mostrar y ocultar.
